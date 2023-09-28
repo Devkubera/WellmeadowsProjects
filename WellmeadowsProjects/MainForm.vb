7bf9fc46e3d012cfcb1361ff6e01700cd9a9064f
KITTIPONG PIADA <kittipongpiada@gmail.com>
2023-09-28 19:57:43 +0700

    Add project files.

---
+++ WellmeadowsProjects/MainForm.vb
@@ -0,0 +1,41 @@
+﻿Public Class MainForm
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+        Controls.Add(New TextBox)
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+
+    Private Sub row1Btn_MouseClick(sender As Object, e As MouseEventArgs) Handles row1Btn.MouseClick
+
+    End Sub
+
+    Private Sub row2Btn_MouseClick(sender As Object, e As MouseEventArgs) Handles row2Btn.MouseClick
+
+    End Sub
+
+    Private Sub row3Btn_MouseClick(sender As Object, e As MouseEventArgs) Handles row3Btn.MouseClick
+
+    End Sub
+
+    Private Sub row4Btn_MouseClick(sender As Object, e As MouseEventArgs) Handles row4Btn.MouseClick
+
+    End Sub
+
+    Private Sub Label5_MouseClick(sender As Object, e As MouseEventArgs) Handles Label5.MouseClick
+
+    End Sub
+
+    Private Sub row6Btn_MouseClick(sender As Object, e As MouseEventArgs) Handles row6Btn.MouseClick
+
+    End Sub
+
+    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
+
+    End Sub
+End Class
